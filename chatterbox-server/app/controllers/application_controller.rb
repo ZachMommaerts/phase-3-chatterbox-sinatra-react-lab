e3b3ec0cb66c4417822f2c2d92f059f6ae0dfc96
Zach <zmommaerts@gmail.com>
2022-01-04 10:26:50 -0600

    stuck

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,31 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    Message.all.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id]).update(
+      body: params[:body],
+      username: params[:username]
+    )
+
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy.to_json
+  end
   
 end
